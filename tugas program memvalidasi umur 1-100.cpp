42b8c7729b7775612a81ae95a1b85c20f93e9648
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 17:57:38 +0700

    Add files via upload

---
+++ tugas program memvalidasi umur 1-100.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    printf("PROGRAM VALIDASI UMUR\n");
+    printf("BY M Achmad Sahroni NIM 311620782\n");
+    printf("KAMPUS PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+	int umur ;
+	cout<<"masukan umurmu :";
+	cin>>umur;
+
+	if (umur>=100)
+	cout<<"pie kabarmu mbah wes mangan durung?"<<endl;
+	cout<<"jadi umur mu : "<<umur<< "tahun";
+}
